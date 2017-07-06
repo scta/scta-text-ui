60f2f537181edf8f8126c4c6c80c5d692174fb07
Jeffrey Witt <jcwitt@JeffsComputer.local>
2017-07-06 17:15:58 -0400

    created basic directory browsing functionality; removed most of the old code in app.rb

--- app.rb
+++ app.rb
@@ -86,48 +86,7 @@ get '/return' do
   redirect '/'
 end
 
-# update (aka commit a file)
-# TODO: make this more generic; most of this is custom for scta texts and repos
-# TODO: abstract some of this out to lib directory
-post '/update' do
-
-  text = @params[:text]
-  sha = @params[:sha]
-  doc = Nokogiri::XML(text).to_xml(:encoding => 'UTF-8')
-  content = Base64.encode64(doc)
-
-  owner = @params[:owner]
-  repo_name = @params[:reponame]
-  item = @params[:resourceid]
-  repo_base = "https://api.github.com/repos"
-  filename = "#{item}.xml"
-  url = "#{repo_base}/#{owner}/#{repo_name}/contents/#{item}/#{filename}"
-  branch = @params[:branch]
-
-  wrapper =
-  {
-  "message": "Update for #{item}",
-  "committer": {
-    "name": "Jeffrey C. Witt",
-    "email": "jeffreycwitt@gmail.com"
-  },
-  "content": content,
-  "sha": sha,
-  "branch": branch
-  }
-  wrapped_content = JSON.pretty_generate(wrapper)
-
-  # create git commit via github api
-  @res = HTTParty.put(url, body: wrapped_content, headers: {'Content-Type' => 'application/json', "Authorization" => "token #{cookies["GITHUB_ACCESS_TOKEN"]}", 'User-Agent' => "scta-text-ui-develop"})
-
-  ## begin pull request
-  pull_url = "#{repo_base}/#{owner}/#{repo_name}/pulls?access_token=#{cookies[:GITHUB_ACCESS_TOKEN]}"
-  submit_pr(pull_url, branch)
-
-  # load new view
-  erb :updated
-end
-
+# STEP 3 return user to main edit page
 # main edit page
 get '/editor' do
   # get api url for target file
@@ -137,15 +96,6 @@ get '/editor' do
   data = client.user
   @username = data.login
   @user_url = data.html_url
-
-  # get doc from github
-  # if url
-  #   @data = get_data(url)
-  #   @doc = get_doc(@data)
-  # else
-  #   @data = {}
-  #   @doc = get_doc_from_template
-  # end
   @access_token = session[:access_token]
   erb :editor
 end
@@ -169,78 +119,4 @@ get '/doc' do
     @doc = get_doc_from_template
   end
   return @doc
-end
-
-get '/data' do
-  content_type :json
-  url = params[:url]
-  data = get_data(url)
-  @data = data.to_json
-end
-# alternative edit page
-# route for edit view with embeded mirador
-# a lot of this is customized to retrieve github file name from scta text id
-# probably best to depreciate this route but preserve it as an example for new route
-get '/edit' do
-
-  @edit_branch_title = if params[:branch] then params[:branch] else "master" end
-
-  # get iiif collection url
-  # get transcription object from scta database
-  item = @params[:resourceid]
-  resource = Lbp::Resource.find(item)
-  if resource.is_a? Lbp::Expression
-    transcription = resource.canonical_transcription.resource
-    @iiif_url = "http://scta.info/iiif/#{resource.top_level_expression.short_id}/collection"
-  elsif resource.is_a? Lbp::Manifestation
-    transcription = resource.canonical_transcription.resource
-    @iiif_url = "http://scta.info/iiif/#{item}/collection"
-  elsif resource.is_a? Lbp::Transcription
-    transcription = resource
-    @iiif_url = "http://scta.info/iiif/#{item}/collection"
-  end
-
-
-  collection_url = "http://scta.info/iiif/"
-
-  repo_array = transcription.doc_path.split("https://").last.split("/")
-  @owner = repo_array[1]
-  @repo = repo_array[2]
-  relative_path = transcription.doc_path.split("/raw/master/").last
-  repo_base = "https://api.github.com/repos"
-  url = "#{repo_base}/#{@owner}/#{@repo}/contents/#{relative_path}"
-
-
-
-  # TODO: replace with functions used in lib/get_functions.rb
-  begin
-    file = open("#{url}?access_token=#{cookies[:GITHUB_ACCESS_TOKEN]}").read
-  rescue OpenURI::HTTPError
-    @data = false
-  else
-    @data = JSON.parse(file)
-    @content = Base64.decode64(@data["content"])
-    @doc = Nokogiri::XML(@content).to_xml(:encoding => 'UTF-8')
-  end
-
-
-  #TODO: this is sort of depreciated, since I'm no longer using two desplays of text
-  branch_file = open("#{url}?ref=#{@edit_branch_title}&access_token=#{cookies[:GITHUB_ACCESS_TOKEN]}").read
-
-  @branch_data = JSON.parse(branch_file)
-  @branch_content = Base64.decode64(@branch_data["content"])
-  @branch_doc = Nokogiri::XML(@branch_content).to_xml(:encoding => 'UTF-8')
-
-  @edit_branch_doc =
-  if params[:branch] == "master" || params[:branch].nil? then
-    if @data != false
-      @doc
-    else
-      @branch_doc
-    end
-  else
-    @branch_doc
-  end
-
-  erb :edit, :layout => false
 end
--- public/assets/javascripts/fileMenu.js
+++ public/assets/javascripts/fileMenu.js
@@ -7,25 +7,31 @@ $(document).ready(function(){
     console.log("test")
     loadTemplateText()
   });
+
+
+//===============BINDING ENVENTS =========================//
 //open dir
-  $(document).on("click","#file-open-dir", function(){
+  $(document).on("click",".file-open-dir", function(){
     $('#save').css({"display": "none"})
+    $('#repo-browser').css({"display": "none"})
+    $('#breadcrumbs').empty();
+    $("#repositories").empty();
     $('#dir').css({"display": "block"})
     var url = "https://api.github.com/user/repos"
-    retrieveAPIData(url, access_token).done(function(data){
-      for (var i = 0, len = data.length; i < len; i++) {
-        $("#repositories").append('<li><a class="file-open-file" data-url="'+ data[i].url + '">' + data[i].url +'</a></li>');
-      }
-    });
+    displayRepoList(url, access_token);
   });
+//open save dialogue box
   $(document).on("click","#file-open-save", function(){
     $('#dir').css({"display": "none"});
+    $('#repo-browser').css({"display": "none"});
     $('#save').css({"display": "block"});
   });
-//open file from dir
+
+// open file events
+  //open file from direct url list
   $(document).on("click",".file-open-file", function(){
     var url = $(this).attr("data-url");
-    $('#dir').css({"display": "none"})
+    $('.file-window').css({"display": "none"})
     loadText(url, access_token)
   });
 // open file from input url
@@ -34,8 +40,45 @@ $(document).ready(function(){
     $('#dir').css({"display": "none"})
     var url = $(this).find("#manual-url").val();
     loadText(url, access_token)
-
   });
+  //open file from directory list
+  $(document).on("click",".file-open-file-list", function(){
+    var path = $(this).attr("data-path");
+    var branch = $(this).attr("data-branch");
+    var branchSha = $(this).attr("data-branch");
+    var repo = $(this).attr("data-repo");
+    var url = "https://api.github.com/repos/" + repo + "/contents" + path;
+    $('.file-window').css({"display": "none"})
+    loadText(url, access_token)
+  });
+//directory or repo opening events
+  //open respository from branch and display top level tree contents
+  $(document).on("click", ".file-open-branch", function(){
+    var url = $(this).attr("data-url");
+    var branch = $(this).attr("data-branch");
+    var branchSha = $(this).attr("data-branch-sha");
+    //retrieveDirectoryCommits(url, access_token)
+    retrieveRepoTree(url, access_token, branch, branchSha);
+  // select repo and liste available branches
+  });
+  $(document).on("click", ".file-open-repo", function(){
+    var url = $(this).attr("data-url");
+    var branch = $(this).attr("data-branch");
+    getRepoBranches(url, access_token);
+  });
+  //display contents of a git tree
+  $(document).on("click",".file-open-tree", function(){
+    var url = $(this).attr("data-url");
+    var path = $(this).attr("data-path");
+    var branch = $(this).attr("data-branch");
+    var branchSha = $(this).attr("data-branch-sha");
+    var repo = $(this).attr("data-repo");
+    retrieveTreeTree(url, access_token, path, branch, branchSha, repo);
+  });
+
+/// FUNCTIONS
+
+// ===========Save Functions =================
 
 // save file using save form data-xmlns
 $("#save-form").submit(function(e){
@@ -77,6 +120,7 @@ $("#save-form").submit(function(e){
         console.log(res.responseJSON.content)
         //updates save parameters; specifically it resets save form with newest shaw
         setSaveParameters(res.responseJSON.content)
+        $('#message').empty();
         $('#save').css({"display": "none"});
       },
       error: function(res, status, error){
@@ -87,6 +131,112 @@ $("#save-form").submit(function(e){
 
 });
 
+//=============DIRECTORY BROWSER FUNCTIONS =================
+function retrieveDirectoryCommits(repo_base, access_token, branch){
+  console.log("TEST")
+  //set branch default to "master"
+  branch = (typeof branch !== 'undefined') ?  branch : "master";
+
+  var url = repo_base + "/commits/" + branch;
+  var url_with_access = url.includes("?") ? url + "&access_token=" + access_token : url + "?access_token=" + access_token;
+  $.ajax({
+    url: url_with_access, // your api url
+    type: 'get', // type is any HTTP method
+    contentType: "application/json",
+    //JSON.stringify seems required; it's the only way I could get it to work
+    success: function(data, status, res) {
+      console.log("direcet directory commits", res)
+      //updates save parameters; specifically it resets save form with newest shaw
+
+
+    },
+    error: function(res, status, error){
+      console.log(res, status, error)
+    }
+  });
+}
+function getRepoBranches(repo_base, access_token){
+  var url = repo_base + "/branches";
+  retrieveAPIData(url, access_token).done(function(data){
+    console.log(data);
+    displayRepoBranches(data, repo_base);
+  });
+}
+function displayRepoBranches(data, repo_base){
+  $("#repo-browser-list").empty();
+  $("#repo-browser").css({"display": "block"});
+  $("#repo-browser-list").append('<h1>Available Branches</h1>');
+  //$("#repo-browser-list").append('<p><a class="file-open-dir">' + "Back to Repo List" +'</a></li>');
+  for (var i = 0, len = data.length; i < len; i++) {
+    $("#repo-browser-list").append('<li><a class="file-open-branch" data-branch-sha="' + data[i].commit.sha + '" data-url="'+ repo_base + '" data-branch="' + data[i].name + '">' + data[i].name +'</a></li>');
+  }
+}
+function retrieveRepoTree(repo_base, access_token, branch, branchSha){
+  //set branch default to "master"
+  var repo = repo_base.split("https://api.github.com/repos/")[1];
+  branch = (typeof branch !== 'undefined') ?  branch : "master";
+  var url = repo_base + "/git/trees/" + branch;
+  var url_with_access = url.includes("?") ? url + "&access_token=" + access_token : url + "?access_token=" + access_token;
+  $.ajax({
+    url: url_with_access, // your api url
+    type: 'get', // type is any HTTP method
+    contentType: "application/json",
+    success: function(data, status, res) {
+      console.log("direcet directory commits", res)
+      displayTree(res.responseJSON.tree, "", branch, branchSha, repo);
+      //updates save parameters; specifically it resets save form with newest shaw
+
+
+    },
+    error: function(res, status, error){
+      console.log(res, status, error)
+    }
+  });
+
+}
+
+function retrieveTreeTree(tree_url, access_token, parent_path, branch, branchSha, repo){
+  retrieveAPIData(tree_url, access_token).done(function(data){
+
+    displayTree(data.tree, parent_path, branch, branchSha, repo, tree_url);
+
+  });
+
+
+}
+function displayRepoList(url, access_token){
+  retrieveAPIData(url, access_token).done(function(data){
+    console.log("repo data", data);
+    $("#repo-browser-branch").empty();
+    for (var i = 0, len = data.length; i < len; i++) {
+      $("#repositories").append('<li><a class="file-open-repo" data-url="'+ data[i].url + '">' + data[i].url +'</a></li>');
+    }
+  });
+}
+
+function displayTree(tree, path, branch, branchSha, repo, parent_tree_url){
+
+  $("#repo-browser-list").empty();
+  $("#repo-browser-branch").empty();
+  $("#repo-browser").css({"display": "block"});
+  $("#repo-browser-branch").html('<p>' + branch + '</p>');
+
+  if (path){
+    //bug, this keeps appending even when you are moving back up the tree
+    $("#breadcrumbs").append(' / <a class="file-open-tree" data-repo="' + repo + '" data-branch="' + branch + '" data-branch-sha="' + branchSha + '" data-url="'+ parent_tree_url + '" data-path="' + path + '">' + path.split("/").pop() +'</a>');
+  }
+  for (var i = 0, len = tree.length; i < len; i++) {
+    if (tree[i].type === 'blob' && tree[i].path.includes('.xml')){
+      $("#repo-browser-list").append('<li><a class="file-open-file-list" data-repo="' + repo + '" data-branch="' + branch + '" data-branch-sha="' + branchSha + '" data-url="'+ tree[i].url + '" data-path="' + path + "/" + tree[i].path + '">' + tree[i].path +'</a></li>');
+    }
+    else if (tree[i].type === 'blob' && !tree[i].path.includes('.xml')){
+      $("#repo-browser-list").append('<li style="color: gray">' + tree[i].path +'</li>');
+    }
+    else if (tree[i].type === 'tree'){
+      $("#repo-browser-list").append('<li><a class="file-open-tree" data-repo="' + repo + '" data-branch="' + branch + '" data-branch-sha="' + branchSha + '" data-url="'+ tree[i].url + '" data-path="' + path + "/" + tree[i].path + '">' + tree[i].path +'</a></li>');
+    }
+  }
+}
 
 // Utility functions
 
@@ -111,7 +261,7 @@ function createPreviewContent(){
 function setSaveParameters(data){
   var sha = data.sha
   var url = data.url.split("?ref=")[0]
-  var branch = data.url.split("?ref=")[1]
+  var branch = data.url.split("?ref=")[1] ? data.url.split("?ref=")[1] : state["branch"];
   $("#sha").attr("value", sha );
   $("#save-url").attr("value", url);
   $("#branch").attr("value", branch );
@@ -134,6 +284,7 @@ function loadTemplateText(){
     $("#preview").html(newText);
   });
 }
+
 
 // note: after a file is saved, if you immediately navigate away and then come back to that file
 //github is sometimes still serviing the file, because it hasn't updated yet.
